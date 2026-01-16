921357e7faa9d0ed5d38178618356b8d6d190849
Gabriel Badjou <gabriel.student.vinci.be>
2026-01-16 16:27:04 +0100

    refactor: function checkPointConditions

--- src/emailValidator.ts
+++ src/emailValidator.ts
@@ -3,8 +3,13 @@ export function emailChecker  (email:string) : boolean {
     if(!email.includes('@')) return false
     let validate = false;
     let index = email.indexOf('@');
-    for(let i = index; i < email.length; i++){
-        if(email[i] === '.' && email[i+1] !== undefined){
+    validate = checkPointConditions(index, email, validate);
+    return validate;
+}
+
+function checkPointConditions(index: number, email: string, validate: boolean) : boolean{
+    for (let i = index; i < email.length; i++) {
+        if (email[i] === '.' && email[i + 1] !== undefined) {
             validate = true;
             break;
         }
